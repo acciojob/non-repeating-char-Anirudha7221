5d852d2b0299ca9df4efbf8fca8923a61db8e3d7
Anirudha Mane <133499217+Anirudha7221@users.noreply.github.com>
2023-07-12 13:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let str=prompt();
+let n=str.length;
+let ans;
+for(let i=0;i<n-1;i++)
+	{
+		if(str.charAt(i)!=str.charAt(i+1))
+		{
+			ans=str.charAt(i);
+		}
+	}
+alert(ans);
